cc5cc6fa42dadc08b9b2be5f48649663e647349f
YodaOfCode <56824947+YodaOfCode@users.noreply.github.com>
2019-12-07 00:54:21 +0200

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,89 @@
+let ipadHover = document.querySelector(".ipad-project");
+ipadHover.onmouseover = function() {
+  let nameOfTheSite = document.querySelector(".ipad-project-name");
+  let buttonOnImg = document.querySelector(".ipad-project-btn");
+  let discription = document.querySelector(".ipad-project-discription");
+  nameOfTheSite.style.display = "block";
+  buttonOnImg.style.display = "flex";
+  discription.style.display = "flex";
+  ipadHover.onmouseout = function() {
+    nameOfTheSite.style.display = "none";
+    buttonOnImg.style.display = "none";
+    discription.style.display = "none";
+  };
+};
+
+let macbook = document.querySelector(".macbook-project");
+macbook.onmouseover = function() {
+  let nameOfTheSite = document.querySelector(".macbook-project-name");
+  let buttonOnImg = document.querySelector(".macbook-project-btn");
+  let discription = document.querySelector(".macbook-project-discription");
+  nameOfTheSite.style.display = "block";
+  buttonOnImg.style.display = "flex";
+  discription.style.display = "flex";
+  macbook.onmouseout = function() {
+    nameOfTheSite.style.display = "none";
+    buttonOnImg.style.display = "none";
+    discription.style.display = "none";
+  };
+};
+
+let business = document.querySelector(".business-project");
+business.onmouseover = function() {
+  let nameOfTheSite = document.querySelector(".business-project-name");
+  let buttonOnImg = document.querySelector(".business-project-btn");
+  let discription = document.querySelector(".business-project-discription");
+  nameOfTheSite.style.display = "block";
+  buttonOnImg.style.display = "flex";
+  discription.style.display = "flex";
+  business.onmouseout = function() {
+    nameOfTheSite.style.display = "none";
+    buttonOnImg.style.display = "none";
+    discription.style.display = "none";
+  };
+};
+
+let games = document.querySelector(".games-project");
+games.onmouseover = function() {
+  let nameOfTheSite = document.querySelector(".games-project-name");
+  let buttonOnImg = document.querySelector(".games-project-btn");
+  let discription = document.querySelector(".games-project-discription");
+  nameOfTheSite.style.display = "block";
+  buttonOnImg.style.display = "flex";
+  discription.style.display = "flex";
+  games.onmouseout = function() {
+    nameOfTheSite.style.display = "none";
+    buttonOnImg.style.display = "none";
+    discription.style.display = "none";
+  };
+};
+
+let technology = document.querySelector(".technology-project");
+technology.onmouseover = function() {
+  let nameOfTheSite = document.querySelector(".technology-project-name");
+  let buttonOnImg = document.querySelector(".technology-project-btn");
+  let discription = document.querySelector(".technology-project-discription");
+  nameOfTheSite.style.display = "block";
+  buttonOnImg.style.display = "flex";
+  discription.style.display = "flex";
+  technology.onmouseout = function() {
+    nameOfTheSite.style.display = "none";
+    buttonOnImg.style.display = "none";
+    discription.style.display = "none";
+  };
+};
+
+let other = document.querySelector(".other-projects");
+other.onmouseover = function() {
+  let nameOfTheSite = document.querySelector(".other-projects-name");
+  let buttonOnImg = document.querySelector(".other-projects-btn");
+  let discription = document.querySelector(".other-projects-discription");
+  nameOfTheSite.style.display = "block";
+  buttonOnImg.style.display = "flex";
+  discription.style.display = "flex";
+  other.onmouseout = function() {
+    nameOfTheSite.style.display = "none";
+    buttonOnImg.style.display = "none";
+    discription.style.display = "none";
+  };
+};
